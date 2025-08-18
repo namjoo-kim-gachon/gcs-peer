dd6ab3169774d5f76d9c6fae338c3e256e35b714
Namjoo Kim <namjookim@gachon.ac.kr>
2025-08-18 19:43:33 +0900

    favicon 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/gcs-logo-light.png" media="(prefers-color-scheme: light)" />
-          <link rel="icon" href="/gcs-logo-dark.png" media="(prefers-color-scheme: dark)" />
+          <link
+            rel="icon"
+            href="/gcs-logo-light.png"
+            media="(prefers-color-scheme: light)"
+          />
+          <link
+            rel="icon"
+            href="/gcs-logo-dark.png"
+            media="(prefers-color-scheme: dark)"
+          />
           <link rel="apple-touch-icon" href="/gcs-logo-light.png" />
         </Head>
         <body>
