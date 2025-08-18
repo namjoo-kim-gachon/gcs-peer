ada5483a0709607eaedf0cb57c90f3657dc1c2d4
Namjoo Kim <namjookim@gachon.ac.kr>
2025-08-18 17:07:12 +0900

    Add _document with light/dark favicons

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/gcs-logo-light.png" media="(prefers-color-scheme: light)" />
+          <link rel="icon" href="/gcs-logo-dark.png" media="(prefers-color-scheme: dark)" />
+          <link rel="apple-touch-icon" href="/gcs-logo-light.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
